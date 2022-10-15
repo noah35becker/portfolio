bb41a6e9b4c4273b7c7206ebb3b3951018b658f6
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-10-15 16:14:41 -0400

    reorder elements in Project component

--- src/components/subpages/Portfolio/Project/index.js
+++ src/components/subpages/Portfolio/Project/index.js
@@ -5,13 +5,15 @@ function Project(){
         <div className="project">
             <a href='' target='_blank'>
                 <h3>Title</h3>
-
-                <img alt="Mad Libs project" src={require(`../../../../assets/project-imgs/${'mad-libs-redaction-unredaction.gif'}`)} />
             </a>
 
             <h5 className="coding-languages">
                 {'SQL, Node (inc. Express), Handlebars'}
             </h5>
+
+            <a href='' target='_blank'>
+                <img alt="Mad Libs project" src={require(`../../../../assets/project-imgs/${'mad-libs-redaction-unredaction.gif'}`)} />
+            </a>
         </div>
     );
 }
