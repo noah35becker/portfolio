e4ad9cd2c237ddcb18c08684d4b5bd09e0dff5e3
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-10-15 12:41:19 -0400

    basic Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+
+// COMPONENT
+function Header(){
+    return (
+        <header>
+            <h1>Noah Becker</h1>
+            
+            <nav>
+                <ul>
+                    <li>About</li>
+
+                    <li>Portfolio</li>
+
+                    <li>Resume</li>
+
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+
+// EXPORT
+export default Header;
