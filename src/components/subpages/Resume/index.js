9fe08f49ea5142d1ec256017797c100eff1ba08d
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2023-01-20 15:39:26 -0500

    adjust sizing of resume `iframe`

--- src/components/subpages/Resume/index.js
+++ src/components/subpages/Resume/index.js
@@ -10,12 +10,11 @@ function Resume(){
     return (<>
         <a className="btn download-btn fs-5 mb-3" href={`https://drive.google.com/uc?export=download&id=${googleDriveFileId}`} download>Download</a>
 
-        <embed
+        <iframe
             className='mb-2'
             src={`https://drive.google.com/file/d/${googleDriveFileId}/preview`}
-            style={{display: 'block', margin: '0 auto', width: '90%', minWidth: '300px', height: '70vw', minHeight: '600px'}}
-            width="75%"
-        />
+            style={{display: 'block', margin: '0 auto', width: '90%', minWidth: '300px', height: '110vw', maxHeight: '1100px', minHeight: '430px'}}
+        ></iframe>
     </>);
 }
 
